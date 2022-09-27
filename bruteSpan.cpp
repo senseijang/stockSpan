471c64393396d738de6c0967af0f61046e9ca427
Jang, Sunwoo <andyjang@iu.edu>
2022-09-27 19:43:37 -0400

    added user input for N

--- bruteSpan.cpp
+++ bruteSpan.cpp
@@ -1,8 +1,42 @@
+/*******************************************************
+ * Filename: bruteSpan.cpp
+ * Author: Andy Jang
+ * Creation Date: 9/27/22
+ * Purpose: To solve the stock span problem using a brute force approach (w/o stack)
+ *******************************************************/
+
+// Pre-processor directives
 #include <iostream>
+
+// Function prototypes
+void checkInput(int N, bool *invalidInput);
 
 int main()
 {
-  std::cout << "Test\n";
+  // Start with getting N
+  bool invalidInput = true;
+  int N = -1;
+
+  while (invalidInput)
+  {
+    std::cout << "N: ";
+    std::cin >> N;
+    std::cout << "\n";
+    checkInput(N, &invalidInput);
+  }
 
   return 0;
+}
+
+void checkInput(int N, bool *invalidInput)
+{
+  if (N < 0)
+  {
+    std::cout << "Stock span is not possible with " << N << ". Please enter a different number.\n";
+  }
+  else
+  {
+    *invalidInput = false;
+    std::cout << "N-value: " << N << "\n";
+  }
 }
