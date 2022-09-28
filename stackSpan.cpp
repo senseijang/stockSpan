dd60709b0548cf811be2cb0242d661f8db0d4908
Jang, Sunwoo <andyjang@iu.edu>
2022-09-28 13:19:35 -0400

    stack stock span done!

--- stackSpan.cpp
+++ stackSpan.cpp
@@ -35,7 +35,7 @@ int main()
 
   // stock span variables and generation
   int stocks[N] = {0};
-  int spans[N] = {0};
+  int spans[N] = {1};
 
   generateStocks(stocks, N);
   getSpans(spans, stocks, N);
@@ -73,7 +73,7 @@ void checkInput(int N, bool *invalidInput)
 void generateStocks(int stocks[], int N)
 {
   // seed for random variables
-  srand(13);
+  srand(time(0));
 
   // generates a random value from 1-9 and assigns it
   for (int i = 0; i < N; i++)
@@ -82,60 +82,44 @@ void generateStocks(int stocks[], int N)
   }
 }
 
+/*
+ * getSpans gets the span values for each stock in a given stock array
+ *
+ * @param spans is the array of spans to be set
+ * @param stocks is the array of randomly generated stocks to be used in calculations
+ * @param N is the number of data entires
+ */
 void getSpans(int spans[], int stocks[], int N)
 {
   std::stack<int> cmp;
-  int topIndex = 0;
   bool keepGoing = true;
 
-  cmp.push(stocks[0]);
+  cmp.push(0);
   spans[0] = 1;
   int i = 1;
 
   while (keepGoing)
   {
-    // new number is biggest on the stack
     if (cmp.empty())
     {
-      cmp.push(stocks[i]);
       spans[i] = i + 1;
-      topIndex = i;
+      cmp.push(i);
       i++;
     }
-    // new number is less than the top
-    else if (stocks[i] < cmp.top())
+    else if (stocks[i] > stocks[cmp.top()])
     {
-      if (cmp.size() >= 2)
-      {
-        topIndex = i - 1;
-      }
-      cmp.push(stocks[i]);
-      spans[i] = i - topIndex;
-      topIndex++;
-      i++;
-    }
-    else if (stocks[i] == cmp.top())
-    {
+      spans[i] = spans[i] + spans[cmp.top()];
       cmp.pop();
-      // topIndex--;
-      cmp.push(stocks[i]);
-
-      // Check for adjacency
-      if (stocks[i] == stocks[i - 1])
-      {
-        spans[i] = 1;
-      }
-      else
-      {
-        spans[i] = i - topIndex;
-      }
-      topIndex = i;
+    }
+    else if (stocks[i] <= stocks[cmp.top()])
+    {
+      spans[i] = spans[i] + 1;
+      cmp.push(i);
       i++;
     }
     else
     {
-      cmp.pop();
-      topIndex--;
+      std::cout << "Error in span calculation!\n";
     }
     if (i == N)
     {
@@ -144,6 +128,13 @@ void getSpans(int spans[], int stocks[], int N)
   }
 }
 
+/*
+ * printArr prints any given array
+ *
+ * @param name is the name of the array
+ * @param arr is the array to be printed
+ * @param N is the size of the array
+ */
 void printArr(std::string name, int arr[], int N)
 {
   std::cout << name << " Array: [ ";
